9d74eeff4d5ec37f3e69460b802fdde5379397eb
Ishika <mishraishika7@gmail.com>
2024-09-09 22:17:52 +0530

    BUG: fixes route import

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    component: import("@/layout/baseLayout.vue"),
+    component: () => import("@/layout/baseLayout.vue"),
     children: [
       {
         path: "/",
